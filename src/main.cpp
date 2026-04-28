27ac5497242dcfccc200922c4a70704f5b526da4
Joey Allen <joey.allen761@gmail.com>
2026-04-28 16:53:17 -0500

    Created robot with tracking

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include <QApplication>
+#include <QInputDialog>
+#include <QString>
 
-#include "model/MessageModel.h"
-#include "view/MainWindow.h"
-#include "controller/AppController.h"
+#include "controller/RobotController.h"
+#include "model/Robot.h"
+#include "view/RobotWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    MessageModel model;
-    MainWindow view;
-    AppController controller(&model, &view);
+    QString robotName = QInputDialog::getText(
+        nullptr,
+        "Robot Name",
+        "Name:"
+    );
+
+    if (robotName.isEmpty()) {
+        robotName = "Robot";
+    }
+
+    Robot model(robotName.toStdString());
+    RobotWindow view;
+    RobotController controller(&model, &view);
 
     controller.initialize();
     view.show();
--- src/model/MessageModel.cpp
+++
@@ -1,9 +0,0 @@
-#include "model/MessageModel.h"
-
-void MessageModel::setMessage(const std::string& newMessage) {
-    message = newMessage;
-}
-
-std::string MessageModel::getMessage() const {
-    return message;
-}
